f7d8904ba4a3c5a28bbc401af83d459f12b6009f
szabojoe <szabo.jozsi29@gmail.com>
2024-05-21 21:46:13 +0200

    Spring Initializr-rel generált projekt

---
+++ runningRace/src/main/java/com/example/runningRace/RunningRaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningRace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningRaceApplication.class, args);
+	}
+
+}
---
+++ runningRace/src/test/java/com/example/runningRace/RunningRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runningRace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
