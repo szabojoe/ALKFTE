155ac0cd41c7bdbde6f9a8c19a02f56fe37a236d
szabojoe <szabo.jozsi29@gmail.com>
2024-05-21 22:52:27 +0200

    addResult

--- runningRace/src/main/java/com/example/runningRace/controller/ResultController.java
+++ runningRace/src/main/java/com/example/runningRace/controller/ResultController.java
@@ -1,12 +1,15 @@
 package com.example.runningRace.controller;
 
 import com.example.runningRace.model.Result;
+import com.example.runningRace.model.Runner;
+import com.example.runningRace.model.Race;
+import com.example.runningRace.repository.RaceRepository;
 import com.example.runningRace.repository.ResultRepository;
+import com.example.runningRace.repository.RunnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +19,33 @@ public class ResultController {
 
     @Autowired
     private ResultRepository resultRepository;
+    @Autowired
+    private RunnerRepository runnerRepository;
+    @Autowired
+    private RaceRepository raceRepository;
 
     @GetMapping("/getRaceRunners/{id}")
     public List<Result> getRaceRunners(@PathVariable Long id) {
         return resultRepository.findByRaceIdOrderByResultTimeAsc(id);
+    }
+
+    @PostMapping("/addResult")
+    public ResponseEntity<String> insertResult(@RequestBody Result newResult) {
+        Long runnerId = newResult.getRunnerId();
+        Long raceId =newResult.getRaceId();
+        Runner existingRunner=runnerRepository.findById(runnerId).orElse(null);
+        Race existingRace=raceRepository.findById(raceId).orElse(null);
+
+        if(existingRace == null || existingRunner == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Race with id: "+ raceId + " or Runner with id: "+ runnerId + " not found");
+        }
+
+        newResult.setResultParticipantId(runnerId);
+        newResult.setResultRaceId(raceId);
+        newResult.setResultTime(newResult.getResultTime());
+        resultRepository.save(newResult);
+
+        return ResponseEntity.ok("Result added successfully");
+
     }
 }
